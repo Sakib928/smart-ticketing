9f6878d679dd25eb863447d1102e5657efa3714d
Sakib928 <sakibgscian@gmail.com>
2024-02-18 21:44:00 +0600

    added seat selection

--- scripts/smart-ticket.js
+++ scripts/smart-ticket.js
@@ -3,3 +3,26 @@ const ticketPage = document.getElementById('bus-routes');
 buy.addEventListener('click', function () {
     ticketPage.scrollIntoView({ behavior: "smooth", block: "start" })
 })
+let seatStatus = [];
+for (let i = 0; i < 40; i++) {
+    seatStatus[i] = 0;
+}
+let bookedSeats = 0;
+let seatsLeft = 40;
+for (let i = 0; i < 40; i++) {
+    let element = document.getElementById(i);
+    element.addEventListener('click', function () {
+        bookedSeats++;
+        seatsLeft--;
+        if (seatStatus[i] == 0 && bookedSeats <= 4) {
+            seatStatus[i] = 1;
+            selectionColor(i);
+            setText('booked-seats', bookedSeats);
+            setText('seats-left', seatsLeft);
+        }
+        if (bookedSeats > 4) {
+            window.alert("Maxium seat limit reached");
+            return 0;
+        }
+    })
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -11,3 +11,21 @@ function buttonEnabler(buttonId) {
     const button = document.getElementById(buttonId).classList;
     button.remove("btn-disabled");
 }
+
+function tableCreate(tableID, seatID) {
+    const table = document.getElementById(tableID);
+    let newTableData = document.createElement('tr');
+    let th1 = document.createElement('th');
+    th1.innerText = document.getElementById(seatID).innerText;
+    newTableData.appendChild(th1);
+    table.appendChild(newTableData);
+    let th2 = document.createElement('th');
+    th2.innerText = "Economy";
+    newTableData.appendChild(th2);
+    table.appendChild(newTableData);
+    let th3 = document.createElement('th');
+    th3.innerText = "550"
+    newTableData.appendChild(th3);
+    table.appendChild(newTableData);
+
+}
